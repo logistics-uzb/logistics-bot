237482849b1cb1e0445dcbc7e7cb3384476a09f4
Mirxonjon <mirxonjonismanov152@gmail.com>
2025-11-05 16:13:02 +0500

    fixed bug

--- src/middleware/verify.js
+++ src/middleware/verify.js
@@ -5,16 +5,18 @@ const { Errorhandler } = require("../exseptions/ErrorHandler");
 dotenv.config();
 
 module.exports = (req, res, next) => {
-    console.log( "access_token0");
+  console.log("access_token0");
 
-  const { access_token } = req.headers;
-    console.log(access_token, "access_token1");
-  if (!access_token) {
+  // Достаём токен из Authorization: Bearer <token>
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(" ")[1];
+  console.log(token, "access_token1");
+
+  if (!token) {
     return next(new Errorhandler("Access token is required", 401));
   }
-  console.log(access_token, "access_token2");
 
-  jwt.verify(access_token, process.env.SECRET_KEY, (err, decoded) => {
+  jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
     console.log(err, "err");
     if (err instanceof jwt.JsonWebTokenError) {
       return next(new Errorhandler("Invalid token", 401));
@@ -23,8 +25,8 @@ module.exports = (req, res, next) => {
     if (!decoded?.id) {
       return next(new Errorhandler("Invalid or missing user ID in token", 401));
     }
-console.log(decoded, "decoded");
 
+    console.log(decoded, "decoded");
     req.id = decoded.id;
     next();
   });
